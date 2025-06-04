d6f2f777b4033ab722f824dc28959d82539fb7d5
Pranav Toggi <prantogg@gmail.com>
2025-06-04 12:25:25 -0700

    inherit UserDict - allows dict and attribute type access

--- tests/test_us.py
+++ tests/test_us.py
@@ -1,7 +1,21 @@
 import wkts
+import pandas as pd
 
 def test_access():
-    print("wkt output:", wkts.us.ca.wkt()[:250] + "...")
-    print("wkt output:", wkts.us.ca.sanfrancisco.wkt()[:250] + "...")
-    print("wkt output:", wkts.us.ny.cityofnewyork.wkt()[:250] + "...")
+    print("Access ID:", wkts.us.id)
+    print("Access region_iso:", wkts.us.region_iso)
+    print("Access region_iso:", wkts.us.ca.region_iso)
+    print("Access name: ", wkts.us.ny.cityofnewyork.name)
+    print("Access subtype:", wkts.us.ca.sanfrancisco.subtype)
+    
+    print(wkts.us.ca.sanfrancisco)
+    print(wkts['us']['ca']['sanfrancisco'])
+
+    sf = wkts.us.ca.sanfrancisco
+    df = pd.DataFrame(sf.data)
+    print(df.head(10))
+    
+    assert(wkts.us.wkt().startswith("MULTIPOLYGON (((-160.4596628 21.8841845"))
     assert(wkts.us.ca.wkt().startswith("MULTIPOLYGON (((-119.621653 33.309252"))
+    assert(wkts.us.ny.cityofnewyork.wkt().startswith("MULTIPOLYGON (((-74.046176 40.691092"))
+    assert(wkts.us.ca.sanfrancisco.wkt().startswith("MULTIPOLYGON (((-122.9915659 37.7672733"))
--- wkts/__init__.py
+++ wkts/__init__.py
@@ -2,93 +2,137 @@ import duckdb
 import importlib.resources
 import sys
 import time
+from collections import UserDict
 
 from . import data
 
 S3_PARQUET_PATH = "s3://overturemaps-us-west-2/release/2025-05-21.0/theme=divisions/type=division_area/*"
 
-class StaticWKL:
-    """Powerhouse behind the Well-Known Locations WKT library. This class
-    represents the string WKT geometry of a given Well-Known Location, but
-    dynamically intercepts attempts to access sub-attributes to resolve
-    locations from the underlying static database."""
+class StaticWKL(UserDict):
+    """Powerhouse behind the Well-Known Locations WKT library. Supports both dict-like and attribute-like access."""
 
-    def __init__(self, name, id=None, country=None, region=None, subtype=None):
-        self.name = name
-        self.id = id
-        self.country = country
-        self.region = region
-        self.subtype = subtype
-        self._geom = None
+    def __init__(self, name, id=None, country_iso=None, region_iso=None, subtype=None):
+        super().__init__()
+        # Always store as lists for consistency
+        self.data["name"] = name if isinstance(name, list) else [name]
+        self.data["id"] = id if isinstance(id, list) else [id]
+        self.data["country_iso"] = country_iso if isinstance(country_iso, list) else [country_iso]
+        self.data["region_iso"] = region_iso if isinstance(region_iso, list) else [region_iso]
+        self.data["subtype"] = subtype if isinstance(subtype, list) else [subtype]
 
-    def __getattr__(self, name):
-        # Only resolve id, not geometry
+    def __getattr__(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            
+            # Country level
+            if not self.data["name"][0]:
+                startT = time.time()
+                country_iso = key.upper()
+                df = duckdb.sql(f"""
+                    SELECT id, name, subtype
+                    FROM wkls
+                    WHERE country = '{country_iso}'
+                      AND subtype = 'country'
+                """).df()
+                if len(df) < 1:
+                    raise AttributeError(f"Country '{country_iso}' not found")
+                endT = time.time()
+                print(f"\nResolved ID(s) of country '{country_iso}' in {endT - startT:.4f} seconds")
+                return StaticWKL(
+                    name=df["name"].tolist(),
+                    id=df["id"].tolist(),
+                    country_iso=[country_iso]*len(df),
+                    subtype=df["subtype"].tolist()
+                )
+            
+            # Region level (State/Province)
+            elif self.data["country_iso"][0] and not self.data["region_iso"][0]:
+                startT = time.time()
+                region_iso = f"{self.data['country_iso'][0]}-{key.upper()}"
+                df = duckdb.sql(f"""
+                    SELECT id, name, subtype
+                    FROM wkls
+                    WHERE country = '{self.data['country_iso'][0]}'
+                      AND region = '{region_iso}'
+                      AND subtype = 'region'
+                """).df()
+                if len(df) < 1:
+                    raise AttributeError(f"Region '{region_iso}' not found")
+                endT = time.time()
+                print(f"Resolved ID(s) of region '{region_iso}' in {endT - startT:.4f} seconds")
+                return StaticWKL(
+                    name=df["name"].tolist(),
+                    id=df["id"].tolist(),
+                    country_iso=[self.data["country_iso"][0]]*len(df),
+                    region_iso=[region_iso]*len(df),
+                    subtype=df["subtype"].tolist()
+                )
+            
+            # County, locality and localadmin levels (County and City)
+            elif self.data["country_iso"][0] and self.data["region_iso"][0]:
+                startT = time.time()
+                df = duckdb.sql(f"""
+                    SELECT id, name, subtype
+                    FROM wkls
+                    WHERE country = '{self.data['country_iso'][0]}'
+                      AND region = '{self.data['region_iso'][0]}'
+                      AND REPLACE(name, ' ', '') ILIKE '{key}'
+                """).df()
+                if len(df) < 1:
+                    raise AttributeError(f"City '{key}' not found.")
+                endT = time.time()
+                print(f"Resolved ID(s) of City/County '{key}' in {endT - startT:.4f} seconds")
+                return StaticWKL(
+                    name=df["name"].tolist(),
+                    id=df["id"].tolist(),
+                    country_iso=[self.data["country_iso"][0]]*len(df),
+                    region_iso=[self.data["region_iso"][0]]*len(df),
+                    subtype=df["subtype"].tolist()
+                )
+            else:
+                raise AttributeError("Only country, region and locality levels are supported in this implementation.")
 
-        # Country level
-        if not self.name:
-            startT = time.time()
-            country_iso = name
-            df = duckdb.sql(f"""
-                SELECT id, country
-                FROM wkls
-                WHERE country = '{country_iso.upper()}'
-                  AND subtype = 'country'
-            """).df()
-            if len(df) != 1:
-                raise AttributeError(f"Country '{country_iso}' not found")
-            endT = time.time()
-            print(f"\nResolved ID of country '{country_iso}' in {endT - startT:.4f} seconds")
-            return StaticWKL(country_iso, id=df["id"][0], country=country_iso.upper(), subtype="country")
+    def __getitem__(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            return self.__getattr__(key)
         
-        # Region level (State/Province)
-        elif self.country and not self.region:
-            startT = time.time()
-            region_code = f"{self.country}-{name.upper()}"
-            df = duckdb.sql(f"""
-                SELECT id, region
-                FROM wkls
-                WHERE country = '{self.country}'
-                  AND region = '{region_code}'
-                  AND subtype = 'region'
-            """).df()
-            if len(df) != 1:
-                raise AttributeError(f"Region '{region_code}' not found")
-            endT = time.time()
-            print(f"Resolved ID of region '{region_code}' in {endT - startT:.4f} seconds")
-            return StaticWKL(name, id=df["id"][0], country=self.country, region=region_code, subtype="region")
-        
-        # County, locality and localadmin levels (County and City)
-        elif self.country and self.region and self.subtype != "city":
-            startT = time.time()
-            df = duckdb.sql(f"""
-                SELECT id, division_id
-                FROM wkls
-                WHERE country = '{self.country}'
-                  AND region = '{self.region}'
-                  AND REPLACE(name, ' ', '') ILIKE '{name}'
-            """).df()
-            if len(df) < 1:
-                raise AttributeError(f"City '{name}' not found.")
-            endT = time.time()
-            print(f"Resolved id of City/County '{self.name}' in {endT - startT:.4f} seconds")
-            return StaticWKL(name, id=df["id"][0], country=self.country, region=self.region, subtype="city")
+    def __setattr__(self, key, value):
+        if key == "data":
+            super().__setattr__(key, value)
         else:
-            raise AttributeError("Only country, region and locality levels are supported in this implementation.")
+            self.data[key] = value
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def items(self):
+        return self.data.items()
+    
+    def keys(self):
+        return self.data.keys()
+
+    def __len__(self):
+        return len(self.data)
 
     def _load_geom(self, transform):
-        if self.id and self.id != "POLYGON()" and self._geom is None:
+        # Always use the first id in the list for geometry
+        id_to_use = self.data["id"][0]
+        if id_to_use and id_to_use != "POLYGON()":
             startT = time.time()
             geom_df = duckdb.sql(f"""
                 SELECT {transform}(geometry) AS geom
                 FROM parquet_scan('{S3_PARQUET_PATH}')
-                WHERE id = '{self.id}'
+                WHERE id = '{id_to_use}'
             """).df()
             if len(geom_df) != 1:
-                raise AttributeError(f"Geometry for id '{self.id}' not found")
-            self._geom = geom_df["geom"][0]
+                raise AttributeError(f"Geometry for id '{id_to_use}' not found")
             endT = time.time()
-            print(f"Looked up '{self.name}' by ID {self.id} in {endT - startT:.4f} seconds")
-        return self._geom
+            print(f"Looked up '{self.data['name'][0]}' by ID {id_to_use} in {endT - startT:.4f} seconds")
+            return geom_df["geom"][0]
+        return None
 
     def wkt(self):
         return str(self._load_geom("ST_AsText")) or ""
